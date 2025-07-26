21c49bd217295a93dfc753b65d0e196d2aef12cf
Diego <diegofsantosr@hotmail.com>
2025-07-26 16:38:20 -0500

    Agrega nombre de amigo al array amigos y limpia el imput

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; // Lista de amigos inicial
+
+
+function agregarAmigo() {
+    let nombre = document.querySelector("#amigo").value;
+    if (nombre === "") {
+        alert("El nombre no puede estar vacío");
+    } else {
+        amigos.push(nombre);
+        document.querySelector("#amigo").value = "";
+        console.log(amigos); 
+    }
+}
